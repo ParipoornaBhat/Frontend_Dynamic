ded9308a5db36e15eb532d839120453d838abb32
ParipoornaBhat <157298696+ParipoornaBhat@users.noreply.github.com>
2025-02-04 20:00:43 +0530

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,7 +6,7 @@ import Login from './pages/Login.jsx';
 import Footer from './components/footer1.jsx'; // Ensure correct casing
 import LoadingScreen from './components/LoadingScreen.jsx'; // Import the LoadingScreen component
 import Profile from './pages/general/Profile.jsx';
-import ChangePassword from './pages/general/ChangePassword';
+import ChangePassword from './pages/general/chpass.jsx';
 import ForgotPassword from './pages/ForgotPassword';
 import ManageUsers from './pages/other/Manageuser.jsx';
 
